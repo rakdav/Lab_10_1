de812fc0e70514f106d6ae24f7dd65dcf6fb4bb3
su <rakdav@inbox.ru>
2022-05-28 11:36:21 +0200

    Добавьте файлы проекта.

---
+++ Lab_10_1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab_10_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MotoCycle motoCycle;
+        public MainWindow()
+        {
+            InitializeComponent();
+            motoCycle = new MotoCycle();
+        }
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files(*.txt)|*.txt";
+            ReadAsync(openFileDialog.FileName);
+        }
+        private async void ReadAsync(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string? line;
+                int i = 1;
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+                    switch(i)
+                    {
+                        case 1:motoCycle.Marka = line; break;
+                        case 2: motoCycle.Color = line; break;
+                        case 3: motoCycle.SerialNumber = int.Parse(line); break;
+                        case 4: motoCycle.RegNumber = line; break;
+                        case 5: motoCycle.Year =int.Parse(line); break;
+                        case 6: motoCycle.YearTech = int.Parse(line); break;
+                        case 7: motoCycle.Price =decimal.Parse(line); break;
+                    }
+                    i++;
+                }
+
+            }
+        }
+    }
+}
---
+++ Lab_10_1/MotoCycle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_10_1
+{
+    internal class MotoCycle
+    {
+        public string Marka { get; set; }
+        public string Color { get; set; }
+        public int SerialNumber { get; set; }
+        public string RegNumber { get; set; }
+        public int Year { get; set; }
+        public int YearTech { get; set; }
+        public decimal Price { get; set; }
+
+        public override string? ToString()
+        {
+            return Marka+"\n"+Color+"\n"+SerialNumber+"\n"+RegNumber+"\n"+
+                Year.ToString()+"\n"+YearTech.ToString()+"\n"+Price.ToString()+"\n";
+        }
+    }
+}
